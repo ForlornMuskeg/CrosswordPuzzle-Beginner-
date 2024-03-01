c0554a432e6d0f2842bc10371bd9b81e3e5b7aff
Umut Can Dundar <161767141+ForlornMuskeg@users.noreply.github.com>
2024-03-01 17:37:20 +0000

    Update VariablesAndLibraries.h
    Comments alignments

--- VariablesAndLibraries.h
+++ VariablesAndLibraries.h
@@ -3,15 +3,15 @@
 #include <iostream>  
 
 #include <iomanip>   /* contains stream manipulators setw(int): to allocate the field to display 
-													 setfill('*'): to fill the allocated but undisplayed field with '*' 
-													 left or right: alignment of the text */
+						     setfill('*'): to fill the allocated but undisplayed field with '*' 
+						     left or right: alignment of the text */
 													
 #include <string>    /* contains getline(): to take the entire line as input including spaces */
 
 #include <conio.h>   /* contains _getch(): to hold the screen until user presses a key */
 
 #include <windows.h> /* contains GetKeyState(key & 0x8000): to get the state whether key is pressed or not by user
-								 SetConsoleTextAttributes(): to change console display colour */
+				 SetConsoleTextAttributes(): to change console display colour */
 
 #include <fstream>   /* filestream: to read information from other files */
 
@@ -22,14 +22,14 @@
 #include <chrono>    /* contains time_point<system_clock>, duration<> */
 
 
-static constexpr int width {80};                                                  /* constant declaration and initialization at compile time(constexpr),
+static constexpr int width {80};    /* constant declaration and initialization at compile time(constexpr),
 																					 it exists during the program life time(static) */
 
 																				  /* time variables declarations */
 std::chrono::time_point<std::chrono::system_clock> StartTime, PauseTime, EndTime; /* to record system_clock at the spesific point of the program */
 std::chrono::duration<double> ElapsedTimeInPause, RealPlayTime;                   /* to find the difference between time_points (by using math operators) */
 
-std::vector <std::string> Questions;											  /* containers declarations */
+std::vector <std::string> Questions;		   /* containers declarations */
 std::map <std::string, std::string> Answers;
 std::map <std::string, std::string> RightAnswers;
 
